a76964ce2fb8503ac23172265bc2ee09bb0cfc0d
ssv32 <ssv_32@mail.ru>
2018-12-02 11:53:24 +0300

    add class iamge

---
+++ gy/class/image.php
@@ -0,0 +1,28 @@
+<?
+/* image class work with image // wrapper class php GD
+ * image класс для работы с изображениями // обёртка класса php GD
+ */
+class image{
+    
+    /* imageResized function compression image (jpeg)
+     * imageResized - сжимает изображения (поддерживает пока jpeg)
+     * in:  $urlImgIn - ссылка на изображение которое нужно сжать // url input image
+     *      $urlImageOut - ссылка куда сохранить изображение // url save image
+     *      $compression - сжатие (0-100) 100 - это наилучнее качество // compression (0-100) 100 max quality
+     */
+    static function imageResized($urlImgIn, $urlImageOut, $compression){
+        $result = false;
+        $arImg = getimagesize($urlImgIn);
+        if ($arImg[2] == 2){// jpeg ли это ? // if jpeg image
+            $img = imageCreateFromJpeg($urlImgIn);// загрузить изображение сжимаемое // loading Image
+            $img2 = imageCreateTrueColor($arImg[0], $arImg[1] ); // создать изображение для сохранение с тем же разрешением // create out image 
+            imageCopyResampled($img2, $img, 0, 0, 0, 0, $arImg[0], $arImg[1], $arImg[0], $arImg[1]); 
+            imageJpeg($img2, $urlImageOut, $compression); // сохраняем // save out image
+            imageDestroy($img2); // очищаем память // clear memory
+            $result = true;
+        }
+        return $result;
+    }
+
+}
+?>

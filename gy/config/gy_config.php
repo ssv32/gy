dee5bc41891120b4c8f3924ae5718df43eeda606
ssv32 <ssv_32@mail.ru>
2020-05-03 17:00:25 +0600

    fix gy_config.php code style

--- gy/config/gy_config.php
+++ gy/config/gy_config.php
@@ -5,14 +5,21 @@ $gy_config = array(
     'lang' => 'rus', // eng
     'sole' => 'pass1021asz#_@)A',
     'db_config' => array(
-        //'db_url' => '/customDir/db',
-        'db_url' => 'C:/OSPanel/domains/demo-gy.lc/customDir/db/',
-        //'db_type' => 'mysql', // mysql | PhpFileSqlClientForGy
-        'db_type' => 'PhpFileSqlClientForGy', // mysql | PhpFileSqlClientForGy
+        
+        //--- example connect mysql---
+        'db_type' => 'mysql', 
         'db_host' => 'localhost',
         'db_user' => 'root', // заменить на настоящего пользователя // replace by true user 
-        'db_pass' => '12345678', // заменить на настоящий пароль // replace by true password
-        //'db_pass' => '', 
+        'db_pass' => '', 
+        //---
+        
+        //--- example connect mysql---
+        //'db_url' => 'C:/OSPanel/domains/demo-gy.lc/customDir/db/',
+        //'db_type' => 'PhpFileSqlClientForGy', 
+        //'db_user' => 'root', // заменить на настоящего пользователя // replace by true user 
+        //'db_pass' => '12345678', // заменить на настоящий пароль // replace by true password
+        //---
+        
         'db_name' => 'gy_db',
     ),
     'type_cache' => 'cacheFiles'

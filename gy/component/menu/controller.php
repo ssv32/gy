5f1e3e3ac479df3868d00cfbcb1c4a915155827f
ssv32 <ssv_32@mail.ru>
2020-05-17 21:46:01 +0600

    fix, /issues/32 - done

--- gy/component/menu/controller.php
+++ gy/component/menu/controller.php
@@ -3,5 +3,9 @@ if ( !defined("GY_GLOBAL_FLAG_CORE_INCLUDE") && (GY_GLOBAL_FLAG_CORE_INCLUDE !==
 
 $arRes['thisUrl'] = $_SERVER['SCRIPT_NAME'];
 
+if(($arRes['thisUrl'] == '/gy/admin/get-admin-page.php') && !empty($_GET['page']) ){
+    $arRes['thisUrl'] = '/gy/admin/get-admin-page.php?page='.htmlspecialchars($_GET['page']);
+}
+
 // показать шаблон
 $this->template->show($arRes, $this->arParam);

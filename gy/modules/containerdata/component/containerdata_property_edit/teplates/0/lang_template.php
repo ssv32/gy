9f0c4c031924b0519cf1873351e5d79bf0f2f802
ssv32 <ssv_32@mail.ru>
2020-09-19 20:38:37 +0300

    fix

--- gy/modules/containerdata/component/containerdata_property_edit/teplates/0/lang_template.php
+++ gy/modules/containerdata/component/containerdata_property_edit/teplates/0/lang_template.php
@@ -15,3 +15,16 @@ $mess['rus'] = array(
     'del-property-ok' => '! свойство этого контейнера данных удалено'
 );
 
+$mess['eng'] = array(
+    'save' => 'Save',
+    'back' => 'Cancel',
+    'not-property' => 'properties not set',
+    'title-add-property' => 'Add new property',
+    'add-property' => 'Add',
+    'add-ok' => 'property added',
+    'add-err' => 'Error! try again',
+    'add-err-not-type' => 'Error! property type not selected',
+    'ok' => 'OK',
+    'del-btn' => 'Delete',
+    'del-property-ok' => '! property of this data container has been removed'
+);
